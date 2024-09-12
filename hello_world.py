db371ee1f19176e23c0a0fc1ba09555823653302
YunJJ-kumoh <yunjj@kumoh.ac.kr>
2024-09-12 17:40:52 +0900

    New Pibonacci code

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,14 @@
 def print_hello():
     print("Hello, World!")
 
-def print_KIT():
-    print("Kumoh National Institute of Technology.")
+def print_pibonacci():
+    a, b, c = 0, 1, 1
+    for _ in range(100):
+        print(c)
+        a = b
+        b = c
+        c = a + b
 
 if __name__ == '__main__':
-    print_KIT()
+    print_pibonacci()
     print_hello()
